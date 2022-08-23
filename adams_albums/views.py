009aae0551b1c7eeea9be6fa82064a6748086a85
Ajsteph89 <ajsteph89@gmail.com>
2022-08-23 15:20:59 -0400

    album details showing

--- adams_albums/views.py
+++ adams_albums/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 # Create your views here.
 def album_list(request):
-    return render(request, 'adams_albums/album_list.html', {})
+    albums = Album.objects.all()
+    return render(request, 'adams_albums/album_list.html', {"albums": albums})
+
+def album_detail(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'adams_albums/album_detail.html', {"album": album})
